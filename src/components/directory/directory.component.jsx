248a04e36af7f872f275af79b731bda05199a5df
souviks72 <souvikmandalvit2016@gmail.com>
2022-09-15 18:24:22 +0530

    adding navigation

--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -5,7 +5,7 @@ const Directory = ({ categories }) => {
   return (
     <div className="directory-container">
       {categories.map((category) => (
-        <CategoryItem category={category} />
+        <CategoryItem key={category.id} category={category} />
       ))}
     </div>
   );
