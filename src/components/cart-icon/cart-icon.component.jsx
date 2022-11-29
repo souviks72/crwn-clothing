9c7684da78ad9c33336dc59f8c12c35d825b44e7
souviks72 <souvikmandalvit2016@gmail.com>
2022-11-29 18:18:00 +0530

    cart migrated to redux

--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -1,19 +1,25 @@
-import { useContext } from 'react';
+import { useSelector, useDispatch } from "react-redux";
 
-import { ReactComponent as ShoppingIcon } from '../../assets/shopping-bag.svg';
+import {
+  selectCartCount,
+  selectIsCartOpen,
+} from "../../store/cart/cart.selector";
+import { setIsCartOpen } from "../../store/cart/cart.action";
 
-import { CartContext } from '../../contexts/cart.context';
+import { ReactComponent as ShoppingIcon } from "../../assets/shopping-bag.svg";
 
-import { CartIconContainer, ItemCount } from './cart-icon.styles';
+import { CartIconContainer, ItemCount } from "./cart-icon.styles";
 
 const CartIcon = () => {
-  const { isCartOpen, setIsCartOpen, cartCount } = useContext(CartContext);
+  const dispatch = useDispatch();
+  const cartCount = useSelector(selectCartCount);
+  const isCartOpen = useSelector(selectIsCartOpen);
 
-  const toggleIsCartOpen = () => setIsCartOpen(!isCartOpen);
+  const toggleIsCartOpen = () => dispatch(setIsCartOpen(!isCartOpen));
 
   return (
     <CartIconContainer onClick={toggleIsCartOpen}>
-      <ShoppingIcon className='shopping-icon' />
+      <ShoppingIcon className="shopping-icon" />
       <ItemCount>{cartCount}</ItemCount>
     </CartIconContainer>
   );
